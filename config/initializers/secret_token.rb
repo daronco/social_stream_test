8950d2a0d741666a51212e5557537111d5f0d2ee
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2011-11-14 18:52:02 -0200

    Rails default app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialStreamTest::Application.config.secret_token = '947cbe029e394de5df040a01a9e1df6f5150ce83ea076a3e1cc36a97cb564d12aadef5a7dfb1e30e0a470465d945be1c1f6624f9b503324e032adc1cca324bcc'
